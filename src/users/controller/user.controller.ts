6c22613bfdcb31076da7c1dd2836c81a44893ed8
Peixotim <pedropeixotovz@gmail.com>
2026-06-15 17:58:32 -0300

    refactor: strongly type user controller response and use HttpStatus

--- src/users/controller/user.controller.ts
+++ src/users/controller/user.controller.ts
@@ -1,17 +1,22 @@
 import {NextFunction, Request, Response} from "express";
 import {UserService} from "../service/user.service";
 import {UserResponseDTO} from "../DTOs/user-response.dto";
+import {HttpStatus} from "../../shared/errors";
 
 export class UserController {
     private service = new UserService();
 
-   public async findByEmail(req : Request, res: Response , next:NextFunction){
-       try{
-            const email : string | string[] = req.params.email
-            const user : UserResponseDTO = await this.service.findByEmail(email as string)
-            return res.status(200).json(user)
-       }catch(error){
+    public async findByEmail(
+        req: Request,
+        res: Response<UserResponseDTO>,
+        next: NextFunction,
+    ): Promise<Response<UserResponseDTO> | void> {
+        try {
+            const email: string = req.params.email as string
+            const user: UserResponseDTO = await this.service.findByEmail(email)
+            return res.status(HttpStatus.OK).json(user)
+        } catch (error) {
             next(error)
-       }
-   }
+        }
+    }
 }
