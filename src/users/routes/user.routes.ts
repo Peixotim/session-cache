a00a55abdb44d4848f508a855d2f72cf1d7ac0a7
Peixotim <pedropeixotovz@gmail.com>
2026-06-15 16:55:27 -0300

    fix: bind controller context in user routes to prevent this loss

--- src/users/routes/user.routes.ts
+++ src/users/routes/user.routes.ts
@@ -1,9 +1,9 @@
-import {Router} from "express";
+import {Router, Request, Response, NextFunction} from "express";
 import {UserController} from "../controller/user.controller";
 
 const router = Router();
 const controller = new UserController();
 
-router.get("/email/:email", controller.findByEmail);
+router.get("/email/:email", (req: Request, res: Response, next: NextFunction) => controller.findByEmail(req, res, next));
 
 export const userRoutes = router;
