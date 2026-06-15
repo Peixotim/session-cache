109f464b7ab8e0ef2c9d8420abe102f2188d3a5d
Peixotim <pedropeixotovz@gmail.com>
2026-06-15 15:38:27 -0300

    feat: bootstrap express app with database init and global middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Happy developing ✨')
+import express from 'express'
+import { AppDataSource } from '../infra/database/database'
+import "reflect-metadata"
+import { errorHandler, notFoundHandler } from './shared/middlewares'
+import { routes } from './routes'
+
+const app = express();
+app.use(express.json())
+
+app.use('/api', routes)
+
+app.use(notFoundHandler)
+app.use(errorHandler)
+
+async function bootStrap(){
+    try{
+        await AppDataSource.initialize()
+        app.listen(8080,()  => {
+            console.log('Api is running in port 8080')
+        })
+    }catch(error){
+        console.error(error);
+    }
+}
+
+bootStrap()
