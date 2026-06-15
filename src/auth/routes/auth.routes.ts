8baeb65c4fb738c87d4281457b6fead2f60dcb79
Peixotim <pedropeixotovz@gmail.com>
2026-06-15 17:58:28 -0300

    feat: validate auth request bodies and bind controller context

--- src/auth/routes/auth.routes.ts
+++ src/auth/routes/auth.routes.ts
@@ -1,10 +1,13 @@
-import {Router, Response, Request, NextFunction} from "express";
+import {Router} from "express";
 import {AuthController} from "../controller/auth.controller";
+import {validateBody} from "../../shared/middlewares";
+import {createUserSchema} from "../../users/DTOs/create-user.dto";
+import {loginSchema} from "../DTOs/login.dto";
 
 const controller = new AuthController()
 const authRoutes = Router()
 
-authRoutes.post("/register", (req : Request, res : Response, next : NextFunction) => controller.register(req, res, next))
-authRoutes.post("/login", (req : Request, res : Response, next : NextFunction) => controller.login(req, res, next))
+authRoutes.post("/register", validateBody(createUserSchema), controller.register.bind(controller))
+authRoutes.post("/login", validateBody(loginSchema), controller.login.bind(controller))
 
 export {authRoutes}
