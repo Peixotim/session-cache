236b9ce1833fb5c30a23693b7a267a0d1cf82ead
Peixotim <pedropeixotovz@gmail.com>
2026-06-15 15:38:10 -0300

    feat: add UserController with create and findByEmail handlers

---
+++ src/users/controller/user.controller.ts
@@ -0,0 +1,17 @@
+import {NextFunction, Request, Response} from "express";
+import {UserService} from "../service/user.service";
+import {UserResponseDTO} from "../DTO/user-response.dto";
+
+export class UserController {
+    private service = new UserService();
+
+   public async findByEmail(req : Request, res: Response , next:NextFunction){
+       try{
+            const email : string | string[] = req.params.email
+            const user : UserResponseDTO = await this.service.findByEmail(email as string)
+            return res.status(200).json(user)
+       }catch(error){
+            next(error)
+       }
+   }
+}
