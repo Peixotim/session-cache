b4f9487e29a230c41abd26bfb78dfce667533b79
Peixotim <pedropeixotovz@gmail.com>
2026-06-15 17:58:32 -0300

    fix: bind health controller context to prevent this loss

--- src/routes/public/health/routes/health.routes.ts
+++ src/routes/public/health/routes/health.routes.ts
@@ -4,7 +4,7 @@ import {HealthController} from "../controller/health.controller";
 const router : Router = Router();
 const controller = new HealthController();
 
-router.get('',controller.HealthCheck)
+router.get('', controller.HealthCheck.bind(controller))
 
 
 export const healthRoutes : Router = router;
