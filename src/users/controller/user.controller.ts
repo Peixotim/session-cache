4dcd67b2ec134299b4a4f08cd4a2f0e8a5f12451
Peixotim <pedropeixotovz@gmail.com>
2026-06-16 13:24:26 -0300

    feat: add getProfile endpoint resolving user by authenticated id

--- src/users/controller/user.controller.ts
+++ src/users/controller/user.controller.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 import {UserService} from "../service/user.service";
 import {UserResponseDTO} from "../DTOs/user-response.dto";
-import {HttpStatus} from "../../shared/errors";
+import {HttpStatus, UnauthorizedError} from "../../shared/errors";
 
 export class UserController {
     private service = new UserService();
@@ -15,6 +15,22 @@ export class UserController {
             const email: string = req.params.email as string
             const user: UserResponseDTO = await this.service.findByEmail(email)
             return res.status(HttpStatus.OK).json(user)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    public async getProfile(
+        req: Request,
+        res: Response<UserResponseDTO>,
+        next: NextFunction,
+    ): Promise<Response<UserResponseDTO> | void> {
+        try {
+            if (!req.user) {
+                throw new UnauthorizedError("Not authenticated.")
+            }
+            const profile: UserResponseDTO = await this.service.getProfile(req.user.sub)
+            return res.status(HttpStatus.OK).json(profile)
         } catch (error) {
             next(error)
         }
