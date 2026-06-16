f70a337495d4b4e6b3bdd7fc9fabdbb650859ce6
Peixotim <pedropeixotovz@gmail.com>
2026-06-16 13:23:36 -0300

    refactor: use validated env module in database datasource

--- infra/database/database.ts
+++ infra/database/database.ts
@@ -1,15 +1,15 @@
 import {DataSource} from "typeorm";
-import {configDotenv} from "dotenv";
 import {User} from "../../src/users/model/user.entity";
-configDotenv();
+import {env} from "../../src/shared/env/env";
+
 
 export const AppDataSource = new DataSource({
     type : 'postgres',
-    host : process.env.DATABASE_HOST,
-    port : Number(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host : env.DATABASE_HOST,
+    port : env.DATABASE_PORT,
+    username: env.DATABASE_USERNAME,
+    password: env.DATABASE_PASSWORD,
+    database: env.DATABASE_NAME,
     synchronize:true,
     entities:[User]
 })
