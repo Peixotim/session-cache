35be05c2b612e96d71cfff7e726991997d4a886c
Peixotim <pedropeixotovz@gmail.com>
2026-06-16 13:24:37 -0300

    feat: register logout, sessions and login-no-session routes

--- src/auth/routes/auth.routes.ts
+++ src/auth/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 import {Router} from "express";
 import {AuthController} from "../controller/auth.controller";
-import {validateBody} from "../../shared/middlewares";
+import {validateBody, AuthMiddleware} from "../../shared/middlewares";
 import {createUserSchema} from "../../users/DTOs/create-user.dto";
 import {loginSchema} from "../DTOs/login.dto";
 
@@ -9,5 +9,8 @@ const authRoutes = Router()
 
 authRoutes.post("/register", validateBody(createUserSchema), controller.register.bind(controller))
 authRoutes.post("/login", validateBody(loginSchema), controller.login.bind(controller))
+authRoutes.post("/login-no-session", validateBody(loginSchema), controller.loginWithoutSession.bind(controller))
+authRoutes.post("/logout", AuthMiddleware, controller.logout.bind(controller))
+authRoutes.get("/sessions", AuthMiddleware, controller.getActiveSessions.bind(controller))
 
 export {authRoutes}
